6b3fda8b4223d142b8c98b165ce608d01cd6b00c
kalle.php <kalle.php@cd68e5fc-efee-11de-91ba-91c70a792394>
2010-10-01 12:18:19 +0000

    Tag Engine 1.0.0

---
+++ 1_0_0/dev/scripts/compile_devtools.php
@@ -0,0 +1,94 @@
+<?php
+	/**
+	 * Tuxxedo Software Engine
+	 * =============================================================================
+	 *
+	 * @author		Kalle Sommer Nielsen 	<kalle@tuxxedo.net>
+	 * @author		Ross Masters 		<ross@tuxxedo.net>
+	 * @version		1.0
+	 * @copyright		Tuxxedo Software Development 2006+
+	 * @license		Apache License, Version 2.0
+	 * @package		Engine
+	 * @subpackage		Dev
+	 *
+	 * =============================================================================
+	 */
+
+
+	use Tuxxedo\Exception;
+	use Tuxxedo\Template;
+
+	define('TEMPLATE_DIR', 		'../tools/style/templates/');
+	define('TUXXEDO_LIBRARY', 	'../../library');
+
+	require(TUXXEDO_LIBRARY . '/Tuxxedo/Exception.php');
+	require(TUXXEDO_LIBRARY . '/Tuxxedo/Exception/TemplateCompiler.php');
+	require(TUXXEDO_LIBRARY . '/Tuxxedo/Template/Compiler.php');
+	require(TUXXEDO_LIBRARY . '/Tuxxedo/Template/Compiler/Dummy.php');
+	require(TUXXEDO_LIBRARY . '/Tuxxedo/functions.php');
+
+	if(isset($_POST['compile']))
+	{
+		$templates = glob(TEMPLATE_DIR . '*.raw');
+
+		if(!$templates)
+		{
+?>
+<p>
+	There is no templates to compile.
+</p>
+<?php
+			exit;
+		}
+?>
+<h2>Compiling ...</h2>
+<ul>
+<?php
+		$compiler = new Template\Compiler;
+
+		$compiler->allowFunction('strlen');
+
+		foreach($templates as $template)
+		{
+			$result 	= 'Failed';
+			$template	= pathinfo($template, PATHINFO_FILENAME);
+
+			try
+			{
+				$compiler->set(file_get_contents(TEMPLATE_DIR . '/' . $template . '.raw'));
+				$compiler->compile();
+
+				file_put_contents(TEMPLATE_DIR . '/' . $template . '.tuxx', $compiler->get());
+
+				$result = 'Success';
+			}
+			catch(Exception\TemplateCompiler $e)
+			{
+			}
+?>
+	<li><?php if(isset($e)) { echo('<strong>'); } echo($template . '... ' . $result); if(isset($e)) { echo(' (' . $e->getMessage() . ')</strong>'); }  ?></li>
+<?php
+			unset($e);
+		}
+?>
+</ul>
+<form action="./compile_devtools.php" method="post">
+	<input type="submit" name="compile" value="Re-Compile" />
+</form>
+<?php
+	}
+	else
+	{
+?>
+<h2>DevTools compiler</h2>
+<p>
+	This tool re-compiles the .raw files into 
+	.tuxx executable files for the development 
+	tools templates.
+</p>
+<form action="./compile_devtools.php" method="post">
+	<input type="submit" name="compile" value="Compile" />
+</form>
+<?php
+	}
+?>
