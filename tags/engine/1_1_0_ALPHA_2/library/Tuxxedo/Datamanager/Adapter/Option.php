f47d471dc6d091d04136ce0f9f36ac49a0c0ad10
kalle.php <kalle.php@cd68e5fc-efee-11de-91ba-91c70a792394>
2011-08-18 03:04:23 +0000

    Tag Engine 1.1.0 Alpha 2

---
+++ tags/engine/1_1_0_ALPHA_2/library/Tuxxedo/Datamanager/Adapter/Option.php
@@ -0,0 +1,204 @@
+<?php
+	/**
+	 * Tuxxedo Software Engine
+	 * =============================================================================
+	 *
+	 * @author		Kalle Sommer Nielsen 	<kalle@tuxxedo.net>
+	 * @author		Ross Masters 		<ross@tuxxedo.net>
+	 * @version		1.0
+	 * @copyright		Tuxxedo Software Development 2006+
+	 * @license		Apache License, Version 2.0
+	 * @package		Engine
+	 * @subpackage		Library
+	 *
+	 * =============================================================================
+	 */
+
+
+	/**
+	 * Datamanagers adapter namespace, this contains all the different 
+	 * datamanager handler implementations to comply with the standard 
+	 * adapter interface, and with the plugins for hooks.
+	 *
+	 * @author		Kalle Sommer Nielsen	<kalle@tuxxedo.net>
+	 * @author		Ross Masters 		<ross@tuxxedo.net>
+	 * @version		1.0
+	 * @package		Engine
+	 * @subpackage		Library
+	 */
+	namespace Tuxxedo\Datamanager\Adapter;
+
+
+	/**
+	 * Aliasing rules
+	 */
+	use Tuxxedo\Exception;
+	use Tuxxedo\Datamanager\Adapter;
+	use Tuxxedo\Datamanager\Hooks;
+	use Tuxxedo\Registry;
+
+
+	/**
+	 * Include check
+	 */
+	defined('\TUXXEDO_LIBRARY') or exit;
+
+
+	/**
+	 * Datamanager for options
+	 *
+	 * @author		Kalle Sommer Nielsen <kalle@tuxxedo.net>
+	 * @version		1.0
+	 * @package		Engine
+	 * @subpackage		Library
+	 */
+	class Option extends Adapter implements Hooks\Cache
+	{
+		/**
+		 * Fields for validation of options
+		 *
+		 * @var		array
+		 */
+		protected $fields		= Array(
+							'option'	=> Array(
+											'type'		=> self::FIELD_REQUIRED, 
+											'validation'	=> self::VALIDATE_STRING
+											), 
+							'value'		=> Array(
+											'type'		=> self::FIELD_REQUIRED, 
+											'validation'	=> self::VALIDATE_STRING_EMPTY
+											), 
+							'defaultvalue'	=> Array(
+											'type'		=> self::FIELD_REQUIRED, 
+											'validation'	=> self::VALIDATE_CALLBACK, 
+											'callback'	=> Array(__CLASS__, 'isValidDefaultValue')
+											), 
+							'type'		=> Array(
+											'type'		=> self::FIELD_REQUIRED, 
+											'validation'	=> self::VALIDATE_CALLBACK, 
+											'callback'	=> Array(__CLASS__, 'isValidType'), 
+											'default'	=> 's'
+											), 
+							);
+
+
+		/**
+		 * Constructor, fetches a new option based on its name if set
+		 *
+		 * @param	\Tuxxedo\Registry		The Registry reference
+		 * @param	integer				The option name
+		 * @param	integer				Additional options to apply on the datamanager
+		 * @param	\Tuxxedo\Datamanager\Adapter	The parent datamanager if any
+		 *
+		 * @throws	\Tuxxedo\Exception\Basic	Throws an exception if the usergroup id is set and it failed to load for some reason
+		 * @throws	\Tuxxedo\Exception\SQL		Throws a SQL exception if a database call fails
+		 */
+		public function __construct(Registry $registry, $identifier = NULL, $options = self::OPT_DEFAULT, Adapter $parent = NULL)
+		{
+			$this->dmname		= 'option';
+			$this->tablename	= \TUXXEDO_PREFIX . 'options';
+			$this->idname		= 'option';
+
+			if($identifier !== NULL)
+			{
+				$option = $registry->db->equery('
+									SELECT 
+										* 
+									FROM 
+										`' . \TUXXEDO_PREFIX . 'options` 
+									WHERE 
+										`option` = \'%s\'', $identifier);
+
+				if(!$option || !$option->getNumRows())
+				{
+					throw new Exception\Basic('Invalid option name passed to datamanager');
+				}
+
+				$this->data 		= $option->fetchAssoc();
+				$this->identifier 	= $identifier;
+
+				$option->free();
+			}
+
+			parent::init($registry, $options, $parent);
+		}
+
+		/**
+		 * Checks whether the default value is valid
+		 *
+		 * @param	\Tuxxedo\Datamanager\Adapter	The current datamanager adapter
+		 * @param	\Tuxxedo\Registry		The Registry reference
+		 * @param	string				The value to check
+		 * @return	boolean				Returns true if the default value is valid
+		 */
+		public static function isValidDefaultValue(Adapter $dm, Registry $registry, $defaultvalue = NULL)
+		{
+			if(!$dm['type'])
+			{
+				return(false);
+			}
+
+			switch(\strtolower($dm['type']{0}))
+			{
+				case('b'):
+				case('s'):
+				{
+					return(true);
+				}
+				case('i'):
+				{
+					return($defaultvalue !== NULL && \is_numeric($defaultvalue));
+				}
+			}
+
+			return(false);
+		}
+
+		/**
+		 * Checks whether the option values fits the type definition
+		 *
+		 * @param	\Tuxxedo\Datamanager\Adapter	The current datamanager adapter
+		 * @param	\Tuxxedo\Registry		The Registry reference
+		 * @param	string				The type to check
+		 * @return	boolean				Returns true if the type is valid, otherwise false
+		 */
+		public static function isValidType(Adapter $dm, Registry $registry, $type = NULL)
+		{
+			static $types;
+
+			if(!$types)
+			{
+				$types = Array('b', 'i', 's');
+			}
+
+			return(\in_array(\strtolower($dm['type']{0}), $types));
+		}
+
+		/**
+		 * Save the option in the datastore, this method is called from 
+		 * the parent class in cases when the save method was success
+		 *
+		 * @param	array				A virtually populated array from the datamanager abstraction
+		 * @return	boolean				Returns true if the datastore was updated with success, otherwise false
+		 */
+		public function rebuild(Array $virtual)
+		{
+			if($this->context == self::CONTEXT_DELETE && !isset($this->registry->cache->options[$this['option']]))
+			{
+				return(true);
+			}
+
+			$option		= (isset($virtual['option']) ? $virtual['option'] : $this['option']);
+			$options 	= $this->registry->cache->options;
+
+			unset($options[$option]);
+
+			if($this->context == self::CONTEXT_SAVE)
+			{
+				$options[$option] = $this['value'];
+			}
+
+			return($this->registry->cache->rebuild('options', $options, false));
+		}
+	}
+?>
---
+++ tags/engine/1_1_0_ALPHA_2/library/Tuxxedo/Exception/SQL.php
@@ -0,0 +1,92 @@
+<?php
+	/**
+	 * Tuxxedo Software Engine
+	 * =============================================================================
+	 *
+	 * @author		Kalle Sommer Nielsen 	<kalle@tuxxedo.net>
+	 * @author		Ross Masters 		<ross@tuxxedo.net>
+	 * @version		1.0
+	 * @copyright		Tuxxedo Software Development 2006+
+	 * @license		Apache License, Version 2.0
+	 * @package		Engine
+	 *
+	 * =============================================================================
+	 */
+
+
+	/**
+	 * Exception namespace, this contains all the core exceptions defined within 
+	 * the library.
+	 *
+	 * @author		Kalle Sommer Nielsen	<kalle@tuxxedo.net>
+	 * @author		Ross Masters 		<ross@tuxxedo.net>
+	 * @version		1.0
+	 * @package		Engine
+	 * @subpackage		Library
+	 */
+	namespace Tuxxedo\Exception;
+
+
+	/**
+	 * Include check
+	 */
+	defined('\TUXXEDO_LIBRARY') or exit;
+
+
+	/*
+	 * SQL Exception
+	 *
+	 * Exception designed to carry error information from a failed 
+	 * query call.
+	 *
+	 * @author		Kalle Sommer Nielsen <kalle@tuxxedo.net>
+	 * @version		1.0
+	 * @package		Engine
+	 */
+	class SQL extends Basic
+	{
+		/**
+		 * Current SQL state
+		 *
+		 * @var		string
+		 */
+		public $sqlstate;
+
+
+		/**
+		 * Constructs a new SQL exception
+		 *
+		 * @param	string			The error that occured
+		 * @param	integer			The associated error number for the error
+		 * @param	string			Optionally, an SQL state if the database driver supports it
+		 */
+		public function __construct($sql, $error, $errno, $sqlstate = NULL)
+		{
+			$this->sql		= $sql;
+			$this->message 		= $error;
+			$this->code		= (integer) $errno;
+			$this->sqlstate		= ($sqlstate ? $sqlstate : false);
+		}
+
+		/**
+		 * Gets the SQL string that caused the exception to trigger
+		 *
+		 * @return	string			Returns the SQL string
+		 */
+		public function getSQL()
+		{
+			return($this->sql);
+		}
+
+		/**
+		 * Gets the current SQL state if the underlaying database 
+		 * driver that threw this exception supports it.
+		 *
+		 * @return	string			Returns the SQL state if supported, otherwise false is returned
+		 */
+		public function getSQLState()
+		{
+			return($this->sqlstate);
+		}
+	}
+?>
