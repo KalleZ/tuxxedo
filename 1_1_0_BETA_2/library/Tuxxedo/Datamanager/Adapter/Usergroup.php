b7bc0cad96e82cf13966503f7b6f67ee973fda79
kalle.php <kalle.php@cd68e5fc-efee-11de-91ba-91c70a792394>
2012-01-16 03:28:08 +0000

    Tag Engine 1.1.0 Beta 2

---
+++ 1_1_0_BETA_2/library/Tuxxedo/Datamanager/Adapter/Usergroup.php
@@ -0,0 +1,166 @@
+<?php
+	/**
+	 * Tuxxedo Software Engine
+	 * =============================================================================
+	 *
+	 * @author		Kalle Sommer Nielsen 	<kalle@tuxxedo.net>
+	 * @author		Ross Masters 		<ross@tuxxedo.net>
+	 * @version		1.0
+	 * @copyright		Tuxxedo Software Development 2006+
+	 * @license		Apache License, Version 2.0
+	 * @package		Engine
+	 * @subpackage		Library
+	 *
+	 * =============================================================================
+	 */
+
+
+	/**
+	 * Datamanagers adapter namespace, this contains all the different 
+	 * datamanager handler implementations to comply with the standard 
+	 * adapter interface, and with the plugins for hooks.
+	 *
+	 * @author		Kalle Sommer Nielsen	<kalle@tuxxedo.net>
+	 * @author		Ross Masters 		<ross@tuxxedo.net>
+	 * @version		1.0
+	 * @package		Engine
+	 * @subpackage		Library
+	 */
+	namespace Tuxxedo\Datamanager\Adapter;
+
+
+	/**
+	 * Aliasing rules
+	 */
+	use Tuxxedo\Datamanager\Adapter;
+	use Tuxxedo\Datamanager\Hooks;
+	use Tuxxedo\Exception;
+	use Tuxxedo\Registry;
+
+
+	/**
+	 * Include check
+	 */
+	\defined('\TUXXEDO_LIBRARY') or exit;
+
+
+	/**
+	 * Datamanager for usergroups
+	 *
+	 * @author		Kalle Sommer Nielsen <kalle@tuxxedo.net>
+	 * @version		1.0
+	 * @package		Engine
+	 * @subpackage		Library
+	 */
+	class Usergroup extends Adapter implements Hooks\Cache
+	{
+		/**
+		 * Fields for validation of usergroups
+		 *
+		 * @var		array
+		 */
+		protected $fields		= Array(
+							'id'		=> Array(
+											'type'		=> self::FIELD_PROTECTED
+											), 
+							'title'		=> Array(
+											'type'		=> self::FIELD_REQUIRED, 
+											'validation'	=> self::VALIDATE_STRING
+											), 
+							'type'		=> Array(
+											'type'		=> self::FIELD_REQUIRED, 
+											'validation'	=> self::VALIDATE_CALLBACK, 
+											'callback'	=> Array(__CLASS__, 'isValidType'), 
+											'default'	=> 2
+											), 
+							'permissions'	=> Array(
+											'type'		=> self::FIELD_OPTIONAL, 
+											'validation'	=> self::VALIDATE_NUMERIC, 
+											'default'	=> 0
+											)
+							);
+
+
+		/**
+		 * Constructor, fetches a new usergroup based on its id if set
+		 *
+		 * @param	\Tuxxedo\Registry		The Registry reference
+		 * @param	integer				The usergroup id
+		 * @param	integer				Additional options to apply on the datamanager
+		 * @param	\Tuxxedo\Datamanager\Adapter	The parent datamanager if any
+		 *
+		 * @throws	\Tuxxedo\Exception\Basic	Throws an exception if the usergroup id is set and it failed to load for some reason
+		 * @throws	\Tuxxedo\Exception\SQL		Throws a SQL exception if a database call fails
+		 */
+		public function __construct(Registry $registry, $identifier = NULL, $options = self::OPT_DEFAULT, Adapter $parent = NULL)
+		{
+			$this->dmname		= 'usergroup';
+			$this->tablename	= \TUXXEDO_PREFIX . 'usergroups';
+			$this->idname		= 'id';
+
+			if($identifier !== NULL)
+			{
+				$usergroup = $registry->db->query('
+									SELECT 
+										* 
+									FROM 
+										`' . \TUXXEDO_PREFIX . 'usergroups` 
+									WHERE 
+										`id` = %d', $identifier);
+
+				if(!$usergroup || !$usergroup->getNumRows())
+				{
+					throw new Exception('Invalid usergroup id passed to datamanager');
+				}
+
+				$this->data 		= $usergroup->fetchAssoc();
+				$this->identifier 	= $identifier;
+
+				$usergroup->free();
+			}
+
+			parent::init($registry, $options, $parent);
+		}
+
+		/**
+		 * Checks whether a usergroup type is valid, this is 
+		 * used as a callback for the validation filter, hence 
+		 * its staticlly defined
+		 *
+		 * @param	\Tuxxedo\Datamanager\Adapter	The current datamanager adapter
+		 * @param	\Tuxxedo\Registry		The Registry reference
+		 * @param	integer				The type to check
+		 * @return	boolean				Returns true if the type is valid, otherwise false
+		 */
+		public static function isValidType(Adapter $dm, Registry $registry, $type)
+		{
+			return($type > 0 && $type < 3);
+		}
+
+		/**
+		 * Save the usergroup in the datastore, this method is called from 
+		 * the parent class in cases when the save method was success
+		 *
+		 * @param	array				A virtually populated array from the datamanager abstraction
+		 * @return	boolean				Returns true if the datastore was updated with success, otherwise false
+		 */
+		public function rebuild(Array $virtual)
+		{
+			if(($datastore = $this->registry->datastore->usergroups) === false)
+			{
+				$datastore = Array();
+			}
+
+			if(!$virtual)
+			{
+				unset($datastore[(integer) ($this->data[$this->idname] ? $this->data[$this->idname] : $this->identifier)]);
+			}
+			else
+			{
+				$datastore[(integer) $this->data[$this->idname]] = $virtual;
+			}
+
+			return($this->registry->datastore->rebuild('usergroups', $datastore));
+		}
+	}
+?>
