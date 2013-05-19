7ec5f8654ac22497a943dff07361ba9454b61628
chhoffbeck <chhoffbeck@cd68e5fc-efee-11de-91ba-91c70a792394>
2013-05-19 18:03:53 +0000

    First woking version of Url.php.

--- branches/engine/PLAYGROUND/mvc/index.php
+++ branches/engine/PLAYGROUND/mvc/index.php
@@ -1,7 +1,10 @@
 <?php
+define('REQUEST_METHOD',	strtolower($_SERVER['REQUEST_METHOD']));
+define('REQUEST_URI',		$_SERVER['REQUEST_URI']);
+
 header('Content-type: text/plain;charset:UTF-8');
 require 'library/Tuxxedo/Mvc.php';
 require 'library/Tuxxedo/Mvc/Url.php';
 require 'App/Routes.php';
-
+#var_dump($_SERVER['REQUEST_URI']);
 //Mvc::init();
