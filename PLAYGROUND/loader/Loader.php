a6b57ff8f3d9e8ad142ca12623059832b73e21e8
chhoffbeck <chhoffbeck@cd68e5fc-efee-11de-91ba-91c70a792394>
2013-11-18 16:32:45 +0000

    Added new autoloader.

---
+++ PLAYGROUND/loader/Loader.php
@@ -0,0 +1,125 @@
+<?php
+	/**
+	 * Tuxxedo Software Engine
+	 * =============================================================================
+	 *
+	 * @author		Christian Hoffbeck		<chhoffbeck@gmail.com>
+	 * @author		Kalle Sommer Nielsen		<kalle@php.net>
+	 * @version		1.0
+	 * @copyright		Tuxxedo Software Development 2006+
+	 * @license		Apache License, Version 2.0
+	 * @package		Engine
+	 * @subpackage		Loader
+	 * 
+	 *
+	 * =============================================================================
+	 */
+
+	 /**
+	  * Tuxxedo namespace
+	  */
+	 namespace Tuxxedo;
+
+	 /**
+	  * Default loader for engine, this will act as a single point of entry for all bundles.
+	  * Any bundles will register themselves or have to be manually registered within this loader.
+	  * 
+	  * <code>
+	  * 	$loader	= new \Tuxxedo\Loader();
+	  * 	$loader->register('Tuxxedo\','library/Engine');
+	  * </code>
+	  * 
+	  * @author		Christian Hoffbeck
+	  * @author		Kalle Sommer Nielsen
+	  * @version		1.0
+	  */
+	 class Loader
+	 {
+	 	/**
+	 	 * A list of registered namespace routes.
+	 	 * All routes is saved in the format of:
+	 	 * namespace => route
+	 	 * 
+	 	 * @var		Array
+	 	 */
+	 	private $namespaces	= [];
+
+	 	/**
+	 	 * Initialize the loader and register the instance as the autoloader autoloader.
+	 	 * 
+	 	 * @param	Array		A list of namespace routes to inject into the autoloader.
+	 	 */ 
+	 	public function __construct(Array $namespaces = NULL)
+	 	{
+	 		spl_autoload_register([$this,'load']);
+	 		if($namespaces)
+	 		{
+	 			$this->namespaces	= array_merge($namespaces,$this->namespaces);
+	 		}
+	 	}
+
+	 	/**
+	 	 * Register a new namespace and folder path.
+	 	 * 
+	 	 * @param	String		Namespace to register.
+	 	 * @param	String		Folder path to the namespace.
+	 	 * @return	void
+	 	 */
+	 	public function register($namespace,$path)
+	 	{
+	 		$this->namespaces[$namespace]	= $path;
+	 	}
+
+	 	/**
+	 	 * Load class, based on the registered namespace.
+	 	 * If no namespace has been registered, 
+	 	 * then the loader will try and use a normlaized path.
+	 	 * 
+	 	 * @param	String		Class name.
+	 	 * @return	Boolean		Returns true if the class is found, otherwise false.
+	 	 */
+	 	public function load($class)
+	 	{	
+	 		$len	= \strlen(\strrchr($class,'\\'));
+	 		$file	= \substr($class,-$len+1);
+	 		$class 	= \substr($class,0,-$len);
+
+	 		if(isset($this->namespaces[$class]))
+	 		{
+	 			$file	= \str_replace(['\\','//'],'/',$class) . \DIRECTORY_SEPARATOR . $file . '.php';
+	 			if(\file_exists($file))
+	 			{
+	 				require $file;
+	 				return(true);
+	 			}
+	 			return(false);
+	 		}
+
+	 		$items	= \explode('\\',$class);
+	 		$found	= false;
+	 		$parts	= '';
+	 		foreach($items AS $part)
+	 		{
+	 			$parts	= $parts . $part;
+	 			if(isset($this->namespaces[$parts]))
+	 			{
+	 				$found	= true;
+	 				break;
+	 			}
+	 			$parts	= $parts . '\\';
+	 		}
+
+	 		if($found === true)
+	 		{
+	 			$file	= \str_replace(['\\','//'],'/',\str_replace($parts,$this->namespaces[$parts],$class) . \DIRECTORY_SEPARATOR . $file . '.php');
+	 		}
+
+
+	 		if(\file_exists($file))
+	 		{
+	 			require $file;
+	 			return(true);
+	 		}
+	 		return(false);
+	 	}
+	 }
