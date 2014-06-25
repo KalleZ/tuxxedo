4c3a74b292141b08783c7d055d82d605f7da774f
kalle.php <kalle.php@cd68e5fc-efee-11de-91ba-91c70a792394>
2014-06-25 19:05:17 +0000

    Tag Tuxxedo Engine 1.2.0 Release Candidate 1

---
+++ tags/1_2_0_RC_1/dev/scripts/includes/bootstrap.php
@@ -0,0 +1,79 @@
+<?php
+	/**
+	 * Tuxxedo Software Engine
+	 * =============================================================================
+	 *
+	 * @author		Kalle Sommer Nielsen 	<kalle@tuxxedo.net>
+	 * @author		Ross Masters 		<ross@tuxxedo.net>
+	 * @version		1.0
+	 * @copyright		Tuxxedo Software Development 2006+
+	 * @license		Apache License, Version 2.0
+	 * @package		Engine
+	 * @subpackage		DevTools
+	 *
+	 * =============================================================================
+	 */
+
+
+	/**
+	 * Aliasing rules
+	 */
+	use Tuxxedo\Registry;
+
+
+	/**
+	 * Sets the path to where the root script is
+	 *
+	 * @var		string
+	 *
+	 * @since	1.1.0
+	 */
+	define('TUXXEDO_DIR', 		realpath(__DIR__ . '/../../..'));
+
+	/**
+	 * Sets the library path
+	 *
+	 * @var		string
+	 *
+	 * @since	1.1.0
+	 */
+	define('TUXXEDO_LIBRARY', 	realpath(__DIR__ . '/../../..') . '/library');
+
+	require(TUXXEDO_LIBRARY . '/configuration.php');
+	require(TUXXEDO_LIBRARY . '/DevTools/functions.php');
+	require(TUXXEDO_LIBRARY . '/Tuxxedo/Loader.php');
+	require(TUXXEDO_LIBRARY . '/Tuxxedo/functions.php');
+
+	date_default_timezone_set('UTC');
+
+	set_error_handler('tuxxedo_error_handler');
+	set_exception_handler('tuxxedo_exception_handler');
+	register_shutdown_function('tuxxedo_shutdown_handler');
+	spl_autoload_register('\Tuxxedo\Loader::load');
+
+	$registry = Registry::init($configuration);
+
+	Registry::globals('error_reporting', 	true);
+	Registry::globals('errors', 		[]);
+
+	$registry->set('timezone', new DateTimeZone('UTC'));
+	$registry->set('datetime', new DateTime('now', $timezone));
+
+	/**
+	 * Set the UTC time constant
+	 *
+	 * @var		integer
+	 *
+	 * @since	1.1.0
+	 */
+	define('TIMENOW_UTC', $datetime->getTimestamp());
+
+	/**
+	 * Set database table prefix constant
+	 *
+	 * @var		string
+	 *
+	 * @since	1.1.0
+	 */
+	define('TUXXEDO_PREFIX', $configuration['database']['prefix']);
+?>
