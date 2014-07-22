0c1492a019287966e17f3c6efbdf6ef4ebc91efb
kalle.php <kalle.php@cd68e5fc-efee-11de-91ba-91c70a792394>
2014-07-22 02:08:52 +0000

    Delete the old bad tag of 1.2.0 RC1

--- tags/1_2_0_RC_1/dev/sql/tuxxedo_sqlite_110_120.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO 'options' ('option', 'value', 'defaultvalue', 'type', 'category') VALUES ('language_autodetect', '0', '0', 'b', 'language');
-
-ALTER TABLE 'sessions' ADD 'rehash' BOOL NOT NULL DEFAULT '0';
